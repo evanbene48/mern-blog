e429c8021883486a3e1c32c52fd230078024dd96
evanbene48 <evanbene48@gmail.com>
2024-02-25 09:25:45 +0700

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,6 +2,7 @@ import User from "../models/user.model.js";
 
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res,next) => {
     const {username, email, password} = req.body;
@@ -42,3 +43,43 @@ export const signup = async (req, res,next) => {
     }
     
 };
+
+
+export const signIn = async (req, res, next) => {
+    const { email, password } = req.body;
+  
+    if (!email || !password || email === '' || password === '') {
+      next(errorHandler(400, 'All fields are required'));
+    }
+  
+    try {
+      const validUser = await User.findOne({ email });
+      if (!validUser) {
+        next(errorHandler(404, 'User not found'));
+      }
+      const validPassword = bcryptjs.compareSync(password, validUser.password);
+      if (!validPassword) {
+        return next(errorHandler(400, 'Invalid password'));
+      }
+
+      //process.env.JWT_SECRET itu adalah
+      //secret key untuk encrypt & decrypt id user di websitenya
+      const token = jwt.sign(
+        { id: validUser._id, isAdmin: validUser.isAdmin },
+        process.env.JWT_SECRET
+      );
+  
+      const { password: loko, ...rest } = validUser._doc;
+
+    //   console.log(validUser._doc)
+  
+      res
+        .status(200)
+        .cookie('access_token', token, {
+          httpOnly: true,
+        })
+        .json(rest);
+    } catch (error) {
+      next(error);
+    }
+  };
