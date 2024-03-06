93fd122ac05ae6a7b27165468549f2fbd4b2c37a
evanbene48 <evanbene48@gmail.com>
2024-03-06 21:17:07 +0700

    make the dashboard private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import Dashboard from './pages/Dashboard';
 import Projects from './pages/Projects';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import PrivateRoute from './components/PrivateRoute';
 
 
 export default function App() {
@@ -20,10 +21,10 @@ export default function App() {
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/sign-up' element={<SignUp />} />
         {/* <Route path='/search' element={<Search />} /> */}
+        
+        <Route element={<PrivateRoute />}>
         <Route path='/dashboard' element={<Dashboard />} />
-        {/* <Route element={<PrivateRoute />}>
-          
-        </Route> */}
+        </Route>
         {/* <Route element={<OnlyAdminPrivateRoute />}>
           <Route path='/create-post' element={<CreatePost />} />
           <Route path='/update-post/:postId' element={<UpdatePost />} />
