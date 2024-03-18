c18463031055cc4a1cd31581556ce6f37a55c08e
evanbene48 <evanbene48@gmail.com>
2024-03-18 21:08:14 +0700

    create getPost API route

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -1,6 +1,4 @@
-import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
-import User from '../models/user.model.js';
 import Post from '../models/post.model.js';
 
 export const createPost = async (req, res,next) => {
@@ -31,3 +29,57 @@ export const createPost = async (req, res,next) => {
     }
 
 };
+
+export const getPosts = async(req,res,next) =>{
+    try {
+        console.log(req.query)
+        const startIndex = parseInt(req.query.startIndex) || 0;
+        const limit = parseInt(req.query.limit) || 9;
+        const sortDirection = req.query.order === 'asc' ? 1 : -1;
+        const posts = await Post.find({
+            //  query for userId
+          ...(req.query.userId && { userId: req.query.userId }),
+            //  query for category
+          ...(req.query.category && { category: req.query.category }),
+            //  query for slug
+          ...(req.query.slug && { slug: req.query.slug }),
+            //  query for postId
+          ...(req.query.postId && { _id: req.query.postId }),
+            //  query for searchTerm
+          ...(req.query.searchTerm && {
+            $or: [
+              { title: { $regex: req.query.searchTerm, $options: 'i' } },
+              { content: { $regex: req.query.searchTerm, $options: 'i' } },
+            ],
+          }),
+        })
+        // sorting di mongodb
+          .sort({ updatedAt: sortDirection })
+          // untuk tahu memulai dari index ke berapa
+          .skip(startIndex)
+          // limit yang dikirim dari database di mongodb
+          .limit(limit);
+    
+        const totalPosts = await Post.countDocuments();
+    
+        const now = new Date();
+    
+        const oneMonthAgo = new Date(
+          now.getFullYear(),
+          now.getMonth() - 1,
+          now.getDate()
+        );
+    
+        const lastMonthPosts = await Post.countDocuments({
+          createdAt: { $gte: oneMonthAgo },
+        });
+    
+        res.status(200).json({
+          posts,
+          totalPosts,
+          lastMonthPosts,
+        });
+      } catch (error) {
+        next(error);
+      }
+}
