40e50377833fb26c9cc2284f013d2881c58d664d
evanbene48 <evanbene48@gmail.com>
2024-03-23 15:11:28 +0700

    add call to the action component to post page

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -93,10 +93,13 @@ export default function PostPage() {
         <div className='p-3 max-w-2xl mx-auto w-full post-content'
             dangerouslySetInnerHTML={{ __html: post && post.content }}
         ></div>
-        <div className='max-w-4xl mx-auto w-full'>
         
-        <CallToAction />
+        {/* Call to Action */}
+        <div className='max-w-4xl mx-auto w-full'>
+            <CallToAction />
         </div>
+        
+        {/* Comment Section */}
         <CommentSection postId={post._id} />
 
         <div className='flex flex-col justify-center items-center mb-5'>
